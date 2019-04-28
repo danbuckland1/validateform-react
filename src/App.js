2b78855265e36178f8c744f2faf344b76a274f4f
danbuckland1 <buckd409@gmail.com>
2019-04-28 17:43:11 -0400

    adding html elemets for components.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        
-          Log In
+        <div className="form-wrapper">
+          <h1>Create Account</h1>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                className=""
+                placeholder="First Name"
+                type="text"
+                name="firstName"
+                noValidate
+                onChange={this.handleChange}
+              />
+            </div>
+          </form>
         </div>
+      </div>
     );
   }
 }
