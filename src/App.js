7be4170a047c4eb9e9d0aaeae03f7c561e3f8c15
danbuckland1 <buckd409@gmail.com>
2019-04-28 18:34:36 -0400

    working on css

--- src/App.js
+++ src/App.js
@@ -20,6 +20,46 @@ class App extends Component {
                 onChange={this.handleChange}
               />
             </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                className=""
+                placeholder="Last Name"
+                type="text"
+                name="lastName"
+                noValidate
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className=""
+                placeholder="Email"
+                type="email"
+                name="email"
+                noValidate
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input
+                type="text"
+                className=""
+                placeholder="Password"
+                type="password"
+                name="password"
+                noValidate
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="createAccount">
+            <button type="submit">Create Account</button>
+            <small>Already Have and Account?</small>
+            </div>
           </form>
         </div>
       </div>
